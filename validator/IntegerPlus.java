248d34e364dd555e97b5c2a157399118502f1be7
Zhc <115857104+ZhcLan@users.noreply.github.com>
2023-12-19 08:10:45 +0800

    Add files via upload
    Update modulates the data set

---
+++ validator/IntegerPlus.java
@@ -0,0 +1,59 @@
+package model.util.type;
+
+import model.range.DataSet;
+import model.range.Range;
+
+import java.util.UUID;
+
+public class IntegerPlus extends ObjectPlus<Integer, IntegerPlus> {
+
+    static {
+        typeStr = IntegerPlus.class.getTypeName();
+        TYPE_PLUS_RANGE = new Range(Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public IntegerPlus() {
+        this(TYPE_PLUS_RANGE);
+    }
+
+    public IntegerPlus(Integer value) {
+        super(value);
+    }
+
+
+    public IntegerPlus(Range range) {
+        this.setUuid(String.valueOf(UUID.randomUUID()));
+        this.value = (int) Range.getRandomMinToMax(range);
+    }
+
+    public IntegerPlus(DataSet set, Range range) {
+        this.setUuid(String.valueOf(UUID.randomUUID()));
+        try {
+            int randomIndex = (int) Range.getRandomMinToMax(range);
+            this.value = (Integer) set.getSet()[randomIndex % set.getSet().length];
+        } catch (Exception e) {
+            throw new IllegalArgumentException("random index out of bound");
+        }
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null && this != null) {
+            return false;
+        }
+
+        if (o == null && this == null) {
+            return true;
+        }
+
+        ObjectPlus cmpObj1 = (ObjectPlus) o;
+        ObjectPlus cmpObj2 = this;
+
+        if (cmpObj1.value.equals(cmpObj2.value)) {
+            return true;
+        }
+
+        return false;
+    }
+}
